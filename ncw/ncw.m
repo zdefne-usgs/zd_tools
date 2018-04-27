415021b630dcdc00189e15b516d14e9c122bcbbc
Zafer <zdefne-usgs@users.noreply.github.com>
2018-04-27 18:36:15 -0400

    Add files via upload

--- ncw/ncw.m
+++ ncw/ncw.m
@@ -1,15 +1,17 @@
-function S=ncw(varargin)
-% A netcdf viewer with ROMS in mind
+function s=ncw(varargin)
+% A netcdf viewer with ROMS in mind. 
 %
-% S=NCW(nc, ptype, vname, tind, lyr, eta, xi);
-% S: structure conatining the answer
-% S.s: variable's values 
-% S.vname: variable's name
-% S.lyr: layer
-% S.t: time
+% s=NCW(nc, ptype, vname, tind, lyr, eta, xi);
+% s: structure conatining the answer
+% q.val: variable's values 
+% q.vname: variable's name
+% q.lyr: layer
+% q.t: time
 %     
-% nc: netcdf file
-% ptype: Plot tyype based on number of dimensions of the variable. Append "m" if masking.
+% nc: Netcdf object. e.g. Using NCTOOLBOX for Matlab:
+%     nc=ncgeodataset(netcdf_file.nc)
+%     nc=ncgeodataset(url_to_netcdf_file.ncml)
+% ptype: Plot type based on number of dimensions of the variable. Append "m" if masking.
 % e.g. '1d', '2d', '3d', '4d' or '2dm', '3dm', '4dm'.
 % vname: name of the variable
 % tind: time steps (for 3-D and 4-D data)
@@ -17,37 +19,33 @@ function S=ncw(varargin)
 % eta: eta points (optional for 3-D and 4-D data, required for 1-D)
 % xi: xi points (optional for 3-D and 4-D data, required for 1-D)
 %
-% Examples: 
-%   ti1=3; ti2=10; eta1=4; eta2=14; xi1=5; xi2=15; lyr1=2; lyr2=4;
-%
 % WITHOUT mask option:
-%   S=ncw(nc, '4d', 'temp', eta1, xi1);
-%   S=ncw(nc, '3d', 'zeta', ti1);
-%   S=ncw(nc, '2d', 'h');
+%   s=ncw(nc, '4d', 'temp', eta1, xi1);
+%   s=ncw(nc, '3d', 'zeta', ti1);
+%   s=ncw(nc, '2d', 'h');
 % 1D salinity time series for time steps ti1 to ti2 at lyr1
-%   S=ncw(nc, '1d', 'salt', ti1:ti2, lyr1, eta1, xi1);
+%   s=ncw(nc, '1d', 'salt', ti1:ti2, lyr1, eta1, xi1);
 % Data is 2D (1 layer) hence lyr=[];
-%   S=ncw(nc, '1d', 'zeta', ti1:ti2, [], eta1, xi1);
+%   s=ncw(nc, '1d', 'zeta', ti1:ti2, [], eta1, xi1);
 % plot the full time series
-%   S=ncw(nc, '1d', 'Dwave', :, 220, 155);
+%   s=ncw(nc, '1d', 'Dwave', :, 220, 155);
 %
 % WITH masking option. '4dm' instead of '4d':
-%   S=ncw(nc, '4dm', 'salt', ti1, 3, eta1:eta2, xi1:xi2);
+%   s=ncw(nc, '4dm', 'salt', ti1, 3, eta1:eta2, xi1:xi2);
 % Slice or vertical profile. '4dp' instead of '4d', slice at xi1:
-%   S=ncw(nc, '4dp', 'salt', ti1, lyr1:lyr2, eta1:eta2, xi1);
+%   s=ncw(nc, '4dp', 'salt', ti1, lyr1:lyr2, eta1:eta2, xi1);
 %
 % 2014, October
 % Zafer Defne
 % --------------------------------------------------------------------
 % Edited to include profile plot option with '4dp'
-%
-% S=ncw(nc, '4dp', 'salt', ti1, lyr1:lyr2, eta1:eta2, xi1);
+%  s=ncw(nc, '4dp', 'salt', ti1, lyr1:lyr2, eta1:eta2, xi1);
 %
 % 2016, December
 % Zafer Defne
 
-nc=varargin{1};
-ptype=varargin{2};
+nc=varargin{1}; 
+ptype=varargin{2}; 
 vname=varargin{3};
 if nargin>3
     tind=varargin{4};
@@ -91,14 +89,27 @@ catch
     end
         t=dstart+nc{'ocean_time'}(tind)/3600/24;
 end
+
 v=nc{vname};
-vatt=v.attributes;
-vstr=vatt(strcmpi(vatt(:,1), 'coordinates') , 2);
-if ~isempty(cell2mat(strfind(vstr, 'lon_rho')))
+%check for coordinates to use to plot
+% ncvars=nc.variables;
+% if(sum(strcmpi(ncvars, 'lon_rho'))<1)
+%    xcoor='x_rho'; ycoor='y_rho';
+% else
+%    xcoor='lon_rho'; ycoor='lat_rho';
+% end
+
+% strcoor=vatt(strcmpi(vatt(:,1), 'coordinates') , 2);
+strcoor=v.attribute('coordinates');
+strind=strfind(v.attribute('coordinates'),' ');
+xcoor=strcoor(1:strind(1)-1)
+ycoor=strcoor(strind(1):strind(2)-1)
+
+if ~isempty(strfind(strcoor, '_rho'))
     mtype='_rho';
-elseif ~isempty(cell2mat(strfind(vstr, 'lon_u')))
+elseif ~isempty(strfind(strcoor, '_u'))
     mtype='_u';
-elseif ~isempty(cell2mat(strfind(vstr, 'lon_v')))
+elseif ~isempty(strfind(strcoor, '_v'))
     mtype='_v';
 end
 switch ptype
@@ -114,7 +125,7 @@ switch ptype
         s=s.*m./m;
         t=t(tind);
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotleft
         title(sprintf('%s\n%s\nlayer %d', vname, datestr(t), lyr),'interpreter', 'none')
     case '4d'
@@ -125,7 +136,7 @@ switch ptype
         end
         t=t(tind);
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotleft
         title(sprintf('%s\n%s\nlayer %d', vname, datestr(t), lyr),'interpreter', 'none')
     case '4dp'
@@ -143,13 +154,13 @@ switch ptype
         if length(eta)>length(xi)+1
             hh=s_rho*h';
             for i=1:length(lyr)+1
-                xh(i,:)=nc{'lat_rho'}(eta, xi);
+                xh(i,:)=nc{ycoor}(eta, xi);
             end
             slicetxt=sprintf('slice at xi = %d', xi);
         else
             hh=s_rho*h;
             for i=1:length(lyr)+1
-                xh(i,:)=nc{'lon_rho'}(eta, xi);
+                xh(i,:)=nc{xcoor}(eta, xi);
             end
             slicetxt=sprintf('slice at eta = %d', eta);
         end
@@ -160,7 +171,7 @@ switch ptype
         ss(end+1,:)=ss(end,:);
         t=t(tind);
         figure
-        pcolor(xh,hh,ss), colorbar
+        pcolor(double(xh),double(hh),double(ss)), colorbar
         plotwide
         title(sprintf('%s\n%s\n%s', vname, datestr(t), slicetxt),'interpreter', 'none')
     case '3dm'
@@ -175,7 +186,7 @@ switch ptype
         s=s.*m./m;
         t=t(tind);
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotmid
         title(sprintf('%s\n%s', vname, datestr(t)),'interpreter', 'none')
     case '3d'
@@ -186,7 +197,7 @@ switch ptype
         end
         t=t(tind);
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotmid
         title(sprintf('%s\n%s', vname, datestr(t)),'interpreter', 'none')
     case '2dm'
@@ -200,7 +211,7 @@ switch ptype
         end
         s=s.*m./m;
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotright
         title(sprintf('%s', vname),'interpreter', 'none')
     case '2d'
@@ -210,7 +221,7 @@ switch ptype
             s(eta,xi)=squeeze(nc{vname}(eta, xi));
         end
         figure
-        pcolor(s), colorbar, axis equal, shading flat
+        pcolor(double(s)), colorbar, axis equal, shading flat
         plotright
         title(sprintf('%s', vname),'interpreter', 'none')
     case '1d'
@@ -222,20 +233,36 @@ switch ptype
         figure
         set(gcf, 'position', [ 580 750 1100 250])
         t=t(tind);
-        plot(t, s, '.-')
-        datetick('x', 'mm/dd HH:MM', 'keepticks')
-        grid on
-        if isempty(lyr)
-            title(sprintf('%s(%d,%d)', vname, eta, xi ),'interpreter', 'none')
-        else
-            title(sprintf('%s(%d,%d,%d)', vname, lyr, eta, xi ),'interpreter', 'none')
+        if length(tind)>1
+            plot(t, s, '.-')
+            datetick('x', 'mm/dd HH:MM', 'keepticks')
+            if isempty(lyr)
+                title(sprintf('%s(%d,%d)', vname, eta, xi ),'interpreter', 'none')
+            else
+                title(sprintf('%s(%d,%d,%d)', vname, lyr, eta, xi ),'interpreter', 'none')
+            end
+        elseif length(xi)>1
+             plot(xi, s, '.-')
+            if isempty(lyr)
+                title(sprintf('%s @eta=%d, t=%s', vname, eta, datestr(t) ),'interpreter', 'none')
+            else
+                title(sprintf('%s @eta=%d, lyr=%s, t=%s', vname, lyr, eta, datestr(t) ),'interpreter', 'none')
+            end
+        elseif length(eta)>1
+             plot(eta, s, '.-')
+            if isempty(lyr)
+                title(sprintf('%s @xi=%d, t=%s', vname, xi, datestr(t) ),'interpreter', 'none')
+            else
+                title(sprintf('%s @xi=%d, lyr=%s, t=%s', vname, lyr, xi, datestr(t) ),'interpreter', 'none')
+            end
         end
+        grid on
 end
 if ~(strcmpi(ptype, '1d') || strcmpi(ptype, '4dp')) && ~isempty(eta) && ~isempty(xi)
     ylim([eta(1),eta(end)])
     xlim([xi(1),xi(end)])
 end
-S.s=s; S.vname=vname; S.lyr=lyr; S.t=t;
+q.val=s; q.vname=vname; q.lyr=lyr; q.t=t;
 
 
 
